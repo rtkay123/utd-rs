2ab68b8701053e29887ab4acbdeca819c7f7b8fc
kawaki <dev@kanjala.com>
2022-03-16 01:15:29 +0200

    refactor: split write to file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::{
 
 use clap::{lazy_static::lazy_static, StructOpt};
 use regex::Regex;
-use tracing::trace;
+use tracing::{debug, trace};
 use utd::{are_you_on_unix, args::PriorityLevel, setup_logger, Task, Tasks};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
@@ -22,46 +22,55 @@ fn main() -> Result<()> {
     // Adding a new note/task
     if args.note.is_some() || args.add.is_some() {
         new_entry(&args)?;
-    } else if args.delete.is_some() {
+    }
+    if args.delete.is_some() {
         delete_entry(&args.delete.unwrap())?;
+    }
+    if args.begin.is_some() {
+        start_tasks(&args.begin.unwrap())?;
     }
 
     Ok(())
 }
 
-fn delete_entry(ids: &[String]) -> Result<()> {
-    let mut path = are_you_on_unix();
-    path.push(".utd.json");
-    let mut tasks: Tasks;
-    {
-        let read_file = std::fs::OpenOptions::new().read(true).open(&path).unwrap();
-        let mut buf_reader = std::io::BufReader::new(read_file);
-        let mut contents = String::new();
-        buf_reader.read_to_string(&mut contents).unwrap();
-        tasks = serde_json::from_str(&contents).unwrap();
-        for i in ids.to_vec().iter() {
-            tasks = tasks
-                .iter()
-                .filter_map(|f| {
-                    if f.id != i.parse::<i64>().unwrap() {
-                        Some(f.to_owned())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-        }
+fn start_tasks(ids: &[String]) -> Result<()> {
+    let mut tasks = state_file_contents()?;
+    for i in ids.iter() {
+        let i: usize = i.parse().unwrap();
+        let vals = tasks
+            .clone()
+            .into_iter()
+            .map(|mut f| {
+                if f.id as usize == i {
+                    f.in_progress = true;
+                    debug!("starting task {}: {}", i, f.name);
+                }
+                f
+            })
+            .collect();
+        tasks = vals;
     }
-    let mut path = are_you_on_unix();
-    path.push(".temp");
-    let mut temp = std::fs::OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(&path)?;
-    write_to_file(&mut temp, &tasks);
-    let mut original = are_you_on_unix();
-    original.push(".utd.json");
-    std::fs::rename(path, original).unwrap();
+    update_file(&tasks)?;
+    debug!("{} tasks started - ok", ids.len());
+    Ok(())
+}
+
+fn delete_entry(ids: &[String]) -> Result<()> {
+    let mut tasks = state_file_contents()?;
+    for i in ids.iter() {
+        tasks = tasks
+            .iter()
+            .filter_map(|f| {
+                if f.id != i.parse::<i64>().unwrap() {
+                    Some(f.to_owned())
+                } else {
+                    None
+                }
+            })
+            .collect();
+    }
+    update_file(&tasks)?;
+    debug!("{} tasks deleted - ok", ids.len());
     Ok(())
 }
 
@@ -77,20 +86,13 @@ fn new_entry(args: &utd::args::Cli) -> Result<()> {
     let entry_adder = |list: &[String],
                        is_task: bool,
                        file: &mut File,
-                       path: &PathBuf,
                        priority: &mut VecDeque<&PriorityLevel>| {
-        let mut tasks: Tasks;
+        let mut tasks: Tasks = state_file_contents().unwrap();
         {
-            let file = std::fs::OpenOptions::new().read(true).open(path).unwrap();
-            let mut buf_reader = std::io::BufReader::new(file);
-            let mut contents = String::new();
-            buf_reader.read_to_string(&mut contents).unwrap();
             // Check if file has data in it
-            if contents.lines().count() > 0 {
-                tasks = serde_json::from_str(&contents).unwrap();
+            if !tasks.is_empty() {
                 trace!("found {} existing tasks", tasks.len());
             } else {
-                tasks = Vec::with_capacity(list.len());
                 trace!("found no existing tasks");
             }
         }
@@ -130,15 +132,15 @@ fn new_entry(args: &utd::args::Cli) -> Result<()> {
 
     let mut vd = VecDeque::from_iter(args.priority.as_ref().unwrap_or(default_vec));
     if let Some(ref tasks) = args.add {
-        entry_adder(tasks, true, &mut file(&path, false, true)?, &path, &mut vd);
+        entry_adder(tasks, true, &mut state_file(&path, false, true)?, &mut vd);
     }
     if let Some(ref notes) = args.note {
-        entry_adder(notes, false, &mut file(&path, false, true)?, &path, &mut vd);
+        entry_adder(notes, false, &mut state_file(&path, false, true)?, &mut vd);
     }
     Ok(())
 }
 
-fn file(path: &PathBuf, read: bool, write: bool) -> Result<File> {
+fn state_file(path: &PathBuf, read: bool, write: bool) -> Result<File> {
     Ok(std::fs::OpenOptions::new()
         .create(true)
         .write(write)
@@ -149,4 +151,33 @@ fn file(path: &PathBuf, read: bool, write: bool) -> Result<File> {
 fn write_to_file(file: &mut File, tasks: &Tasks) {
     writeln!(file, "{}", serde_json::to_string_pretty(tasks).unwrap()).unwrap();
     trace!("tasks updated");
+}
+
+fn state_file_contents() -> Result<Tasks> {
+    let mut path = are_you_on_unix();
+    path.push(".utd.json");
+    let read_file = std::fs::OpenOptions::new().read(true).open(&path).unwrap();
+    let mut buf_reader = std::io::BufReader::new(read_file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    // empty list
+    if contents.is_empty() {
+        contents.push_str("[]");
+    }
+    let tasks: Tasks = serde_json::from_str(&contents)?;
+    Ok(tasks)
+}
+
+fn update_file(tasks: &Tasks) -> Result<()> {
+    let mut path = are_you_on_unix();
+    path.push(".temp");
+    let mut temp = std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(&path)?;
+    write_to_file(&mut temp, tasks);
+    let mut original = are_you_on_unix();
+    original.push(".utd.json");
+    std::fs::rename(path, original)?;
+    Ok(())
 }
