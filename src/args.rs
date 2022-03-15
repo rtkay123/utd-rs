75efbd67af93291ed208a2714bc01d0d572935d7
kawaki <dev@kanjala.com>
2022-03-15 14:59:09 +0200

    feat: parse cli args

---
+++ src/args.rs
@@ -0,0 +1,47 @@
+use clap::{ArgEnum, Parser};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct Cli {
+    /// Add note(s)
+    #[clap(short, long)]
+    pub note: Option<Vec<String>>,
+
+    /// Add a new task(s)
+    #[clap(short, long)]
+    pub add: Option<Vec<String>>,
+
+    /// Check/uncheck task(s) as complete
+    #[clap(short, long)]
+    pub check: Option<Vec<String>>,
+
+    /// Start/stop task(s)
+    #[clap(short, long)]
+    pub begin: Option<Vec<String>>,
+
+    /// Sort all tasks and notes
+    #[clap(short, long, arg_enum)]
+    pub sort: Option<SortParam>,
+
+    /// Clear all completed notes/tasks
+    #[clap(short, long)]
+    pub tidy: bool,
+
+    /// Set a priority level
+    #[clap(short, long, arg_enum)]
+    pub priority: Option<PriorityLevel>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum PriorityLevel {
+    Low,
+    Medium,
+    High,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum SortParam {
+    Time,
+    Priority,
+    Id,
+}
